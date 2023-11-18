df83dee624f4d0a2be3c9b494bf273c7729f131e
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-11-18 10:46:55 +0100

    setup react and install packages

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from './components/Form'
+export default function App() {
+  return (
+    <>
+    <div>hi</div>
+    <Form />
+    </>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+  },
+};
