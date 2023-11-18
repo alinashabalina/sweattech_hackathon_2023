ffd5eb4a6cfcce1b510a94aec0847e6981179200
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-11-18 14:45:36 +0100

    make calendar clickable but not working yet

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {DateCalendar} from '@mui/x-date-pickers/DateCalendar';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
 
 export default function Calendar() {
+  const [value, setValue] = useState(dayjs)
+  console.log(value);
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar />
+        <DateCalendar value={value} onChange={(newValue)=>setValue(newValue)} />
     </LocalizationProvider>
   )
 }
